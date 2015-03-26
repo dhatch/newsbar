57e10911b11f0791a7c629ab0fd5aae56af478d6
David Hatch <dhatch387@gmail.com>
2015-03-25 22:41:17 -0400

    Add upvotes to icon.

--- extension/hbar.js
+++ extension/hbar.js
@@ -14,9 +14,13 @@ var findItemForPage = function (url, callback) {
             if (request.status === 200) {
                 var js = JSON.parse(request.responseText);
                 if (js.hits && js.hits.length) {
-                    var hit = js.hits[0];
-                    if (hit.url === url) {
-                        return callback(true, hit.objectID);
+                    var hit = js.hits[0],
+                          hita = document.createElement('a'),
+                          pga  = document.createElement('a');
+                    hita.href = hit.url;
+                    pga.href = url;
+                    if (hita.hostname === pga.hostname) {
+                        return callback(true, hit.objectID, hit.points);
                     }
                 }
             }
@@ -28,9 +32,10 @@ var findItemForPage = function (url, callback) {
 };
 
 var hbarForPage = function (url, tabId) {
-    findItemForPage(url, function (exists, hnId) {
+    findItemForPage(url, function (exists, hnId, hnPoints) {
         var tabStr = Number(tabId).toString();
         if (exists) {
+            setPageActionIcon(tabId, Number(hnPoints).toString());
             chrome.pageAction.show(tabId);
             var data = {};
             data[tabStr] = 'https://news.ycombinator.com/item?id='+hnId;
@@ -44,6 +49,50 @@ chrome.webNavigation.onCommitted.addListener(function (ev) {
     if (ev.frameId === 0)
         hbarForPage(ev.url, ev.tabId);
 });
+
+var setPageActionIcon = function (tab, text) {
+    /* See:
+     * http://chat.stackoverflow.com/transcript/message/13972713#13972713
+     */
+    var canvas19 = document.createElement('canvas'),
+        canvas38 = document.createElement('canvas'),
+        img19 = document.createElement('img'),
+        img38 = document.createElement('img'),
+        id19 = null,
+        id38 = null,
+        draw = function (size, canvas, img) {
+            return function () {
+                /* Draw the background image */
+                var context = canvas.getContext('2d');
+                context.drawImage(img, 0, 0, size, size);
+
+                /* Draw some text */
+                context.fillStyle = "white";
+                var fontSize = size == 19 ? 8 : 16;
+                context.font = "bold "+ fontSize +"px Sans-Serif";
+                var tw = context.measureText(text);
+                context.fillText(text, (size - tw.width)/2, size - 1, size);
+                dest = context.getImageData(0, 0, size, size);
+
+                if (size == 19)
+                    id19 = dest;
+                else
+                    id38 = dest;
+
+                if (id19 && id38) {
+                    chrome.pageAction.setIcon({
+                        imageData: {'19': id19, '38': id38 },
+                        tabId:     tab
+                    });
+                }
+            };
+        };
+
+    img19.onload = draw(19, canvas19, img19);
+    img38.onload = draw(38, canvas38, img38);
+    img19.src = "images/hn-19.png";
+    img38.src = "images/hn-38.png";
+};
 
 chrome.pageAction.onClicked.addListener(function (tab) {
     var tabString = Number(tab.id).toString();
